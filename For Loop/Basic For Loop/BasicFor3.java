60c24c7b05830612d0ae8289a45860c809ca1e41
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-04-19 08:11:47 +0530

    Basic For Loop

---
+++ For Loop/Basic For Loop/BasicFor3.java
@@ -0,0 +1,11 @@
+class BasicFor3{
+
+        public static void main(String[] args){
+                int N=109;
+                for(int i=100;i<=N;i++){
+
+                        System.out.println(i);
+
+                }
+        }
+}
