97dcc12f3cd718c28e3afe5dc8fa888bd59284dc
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-04-23 10:07:13 +0530

    For Loop Basic

---
+++ For Loop/Basic For Loop/BasicFor7.java
@@ -0,0 +1,11 @@
+class BasicFor7{
+
+        public static void main(String[] args){
+                int N=10;
+                for(int i=1;i<=N;i++){
+		
+                                System.out.println(12*i);
+
+                }
+        }
+}
