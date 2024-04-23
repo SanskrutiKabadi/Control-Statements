df7deee102c917af2dd9fe685c2a593c07ece0a1
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-04-23 10:08:05 +0530

    For Loop Basic

---
+++ For Loop/Basic For Loop/BasicFor9.java
@@ -0,0 +1,12 @@
+class BasicFor9{
+
+        public static void main(String[] args){
+                int N=10;
+		int sum=0;
+                for(int i=1;i<=N;i++){
+			sum=sum+i;
+
+                }
+		 System.out.println("Sum = "+sum);
+        }
+}
