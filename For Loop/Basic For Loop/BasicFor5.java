c3545afc220e3241d5792771b978a8ced501feda
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-04-23 10:06:27 +0530

    For Loop Basic

---
+++ For Loop/Basic For Loop/BasicFor5.java
@@ -0,0 +1,11 @@
+class BasicFor5{
+
+        public static void main(String[] args){
+                int N=50;
+                for(int i=1;i<=N;i++){
+                        if(i%2!=0){
+                                System.out.println(i);
+                        }
+                }
+        }
+}
