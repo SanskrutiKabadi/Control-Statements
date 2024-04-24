3bf46101606cf1fd02f972c4f4e326a4c2e9d38a
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-04-24 13:30:24 +0530

    Basic For Loop

---
+++ For Loop/Basic For Loop/BasicFor10.java
@@ -0,0 +1,12 @@
+class BasicFor10{
+
+        public static void main(String[] args){
+                int N=10;
+                int product=1;
+                for(int i=1;i<=N;i++){
+                        product=product*i;
+
+                }
+                 System.out.println("product = "+product);
+        }
+}
