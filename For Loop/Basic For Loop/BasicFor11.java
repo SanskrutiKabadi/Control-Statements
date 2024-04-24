ff6e32ef02656fe0d3be799b8d98f05e7a80b9dd
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-04-24 13:31:02 +0530

    Basic For Loop

---
+++ For Loop/Basic For Loop/BasicFor11.java
@@ -0,0 +1,12 @@
+class BasicFor11{
+
+        public static void main(String[] args){
+                int N=5;
+                int factorial=1;
+                for(int i=1;i<=N;i++){
+                        factorial=factorial*i;
+
+                }
+                 System.out.println("factorial = "+factorial);
+        }
+}
